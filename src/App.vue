be0afad44eab0bda2f89ee7707959e955884fdf1
Knara Kochkanian <knarak@yandex.ru>
2022-07-12 11:54:55 +0400

    Finish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="container-top">
+      <p>Мой ИЖС</p>
+      <div>
+        <div class="container-top__img">
+        </div>
+        <p>статус</p>
+      </div>
+    </div>
+    <div class="info-top">
+      <p>Все инженеры и Росреестр за 0 ₽ — оплатите первый этап строительства до 25 августа и Genius активируется
+        автоматически
+      </p>
+      <img src="./assets/G.svg" alt="G">
+    </div>
+    <div class="info-menu">
+      <button>
+        <div>
+          <div class="info-menu_img _1"></div>
+          <p>Как этo работает</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _2"></div>
+          <p>ИЖС Стандарт</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _3"></div>
+          <p>Договоры</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _4"></div>
+          <p>Росреестр</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _5"></div>
+          <p>Помощь</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _6"></div>
+          <p>Промокоды</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _7"></div>
+          <p>Партнёрам</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+      <button>
+        <div>
+          <div class="info-menu_img _8"></div>
+          <p>Избранное</p>
+        </div>
+        <img src="./assets/arrow.svg" alt="arrow">
+      </button>
+    </div>
+    <footer class="footer">
+      <button>
+        <img src="./assets/main.svg" alt="main">
+        <p>Главная</p>
+      </button>
+      <button>
+        <img src="./assets/katalog.svg" alt="katalog">
+        <p>Каталог</p>
+      </button>
+      <button>
+        <img src="./assets/land.svg" alt="land">
+        <p>Landscape</p>
+      </button>
+      <button>
+        <img src="./assets/desk.svg" alt="desk">
+        <p>Стройка</p>
+      </button>
+      <button>
+        <img src="./assets/menu.svg" alt="menu">
+        <p>Меню</p>
+      </button>
+
+
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./components/menu.scss" lang="scss"></style>
